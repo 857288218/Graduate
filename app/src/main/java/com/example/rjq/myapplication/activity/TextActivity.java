66737aa122204d5b5f36875eee811bcac178c14e
rjq <857288218@qq.com>
2017-10-30 12:27:41 +0800

    onDestroy

--- app/src/main/java/com/example/rjq/myapplication/activity/TextActivity.java
+++ app/src/main/java/com/example/rjq/myapplication/activity/TextActivity.java
@@ -84,6 +84,11 @@ public class TextActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
     public void onClick(View v) {
         switch (v.getId()){
 
